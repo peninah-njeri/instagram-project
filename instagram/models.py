36c8a10690401a8ac277845fe74a3e6e04ce9c9b
peninah-njeri <peninahgathuru@gmail.com>
2019-11-17 18:31:23 +0300

    create a user authentication system to register new users

--- instagram/models.py
+++ instagram/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Image(models.Model):
-    image = models.ImageField(upload_to =)
+    image = models.ImageField()
     image_name = models.CharField(max_length=30)
     image_caption = models.TextField()
     profile = models.ForeignKey('Profile',on_delete=models.CASCADE)
     comments = models.TextField()
 
 class Profile(models.Model):
-    profile_photo = models.ImageField(upload_to ='pic_folder/',)
+    profile_photo = models.ImageField()
     bio = models.TextField()    
 
 
