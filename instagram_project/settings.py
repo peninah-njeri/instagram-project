4c4342c966d22734a2860d9450d3d52d8068f707
peninah-njeri <peninahgathuru@gmail.com>
2019-11-18 17:50:57 +0300

    adds users app models and routes

--- instagram_project/settings.py
+++ instagram_project/settings.py
@@ -31,8 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'instagram.apps.InstagramConfig',
-    'bootstrap3',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- instagram_project/urls.py
+++ instagram_project/urls.py
@@ -13,14 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+
 from django.contrib import admin
-from django.contrib.auth import views 
+# from django.urls import path
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+from users import views as users_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('instagram.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/'}),
-
+    url(r'^profile/', users_views.profile, name="profile"),
+    url(r'^register$', users_views.register, name="register"),
+    url(r'^login/&',auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    url(r'^logout/&',auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    url(r'^', include('articles.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
