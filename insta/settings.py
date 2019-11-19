2ece9560b5c785ab8e1f10f5e30cef8dbf0fc668
peninah-njeri <peninahgathuru@gmail.com>
2019-11-19 09:39:43 +0300

    modified settings for deploymnt to heroku

--- insta/settings.py
+++ insta/settings.py
@@ -180,11 +180,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '1018.laflare@gmail.com'
-EMAIL_HOST_PASSWORD = 'Laflare1020'
-EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = '1018.laflare@gmail.com'
+# EMAIL_HOST_PASSWORD = 'Laflare1020'
+# EMAIL_PORT = 587
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
